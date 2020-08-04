4526d28f60a73aab60acc1349a3e8141d797fa44
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 02:55:44 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,12 +5,7 @@ end
 
 
 
-
+#generator: 
 #rails g model Appointment_datetime:datetime created_at:datetime updated_at:datetime doctor_id:integer patient_id:integer  --no-test-framework --skip-routes 
 
 
-t.datetime :appointment_datetime
-t.datetime :created_at
-t.datetime :updated_at
-t.integer :doctor_id
-t.integer :patient_id
