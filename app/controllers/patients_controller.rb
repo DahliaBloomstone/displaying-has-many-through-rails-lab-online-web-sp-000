c0a32fd97c1020e48f4b72866ab6a723f3c4da07
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 02:58:46 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,10 +1,11 @@
 class PatientsController < ApplicationController
 
-#display patients on index page 
+#display patients on index page
 def index
   @patients = Patient.all
 end
 
+#show patient by ID 
 def show
   @patient = Patient.find(params[:id])
   end
